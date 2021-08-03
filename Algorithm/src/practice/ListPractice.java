bc96e7a4a66bb09924e1a8172b4d3a42ec45694d
jeffkim <jeffkim4012@gmail.com>
2021-08-02 23:49:11 -0400

    yelp final

---
+++ Algorithm/src/practice/ListPractice.java
@@ -0,0 +1,27 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListPractice {
+
+    public static void main(String[] args) {
+        User a = new User("a");
+        User b = new User("b");
+        List<User> list = new ArrayList<User>();
+        list.add(a);
+        list.add(b);
+        list = List.of(a, b);
+
+        list.remove(a);
+
+    }
+
+    static class User {
+        String name;
+
+        public User(String name) {
+            this.name = name;
+        }
+    }
+}
--- Algorithm/src/pro/SectionSum_2042_3.java
+++ Algorithm/src/pro/SectionSum_2042_3.java
@@ -30,48 +30,27 @@ public class SectionSum_2042_3 {
 
         init(1, 1, numCnt);
 
-        while (cnt-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int formular = Integer.parseInt(st.nextToken());
-            int idx = Integer.parseInt(st.nextToken());
-            int val = Integer.parseInt(st.nextToken());
-
-            if (formular == 1) {
-                int diff = val - arrInput[idx];
-                arrInput[idx] = val;
-                update(1, 1, numCnt, idx, diff);
-            } else {
-                int sum = sum(1, 1, numCnt, idx, val);
-                bw.write(sum+"\n");
-            }
-        }
-
         br.close();
         bw.close();
-
     }
 
     static int init(int idx, int s, int e) {
-        if (s >= e) {
+        if (s >= e)
             return tree[idx] = arrInput[s];
-        }
 
         int mid = (s + e) / 2;
         return tree[idx] = init(idx * 2, s, mid) + init(idx * 2 + 1, mid + 1, e);
     }
 
     static void update(int idx, int s, int e, int loc, int diff) {
-        if (loc < s || e < loc) {
+        if (loc < s || e < loc)
             return;
-        }
 
-        if (s <= loc && loc <= e) {
+        if (s <= loc && loc <= e)
             tree[idx] += diff;
-        }
 
-        if (s >= e) {
+        if (s >= e)
             return;
-        }
 
         int mid = (s + e) / 2;
         update(idx * 2, s, mid, loc, diff);
@@ -79,17 +58,14 @@ public class SectionSum_2042_3 {
     }
 
     static int sum(int idx, int s, int e, int l, int r) {
-        if (l <= s && e <= r) {
+        if (l <= s && e <= r)
             return tree[idx];
-        }
 
-        if (l > e || r < s) {
+        if (l > e || r < s)
             return 0;
-        }
 
-        if (s >= e) {
+        if (s >= e)
             return 0;
-        }
 
         int mid = (s + e) / 2;
         return sum(idx * 2, s, mid, l, r) + sum(idx * 2 + 1, mid + 1, e, l, r);
